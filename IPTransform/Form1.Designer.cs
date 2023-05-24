92b91a2fb656208fd6c2563ab923dbea726dec63
SOEMTECH <SOEMTECH@DESKTOP-3827NLF>
2023-05-24 22:15:57 +0300

    Add project files.

---
+++ IPTransform/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace IPTransform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Btn_Set_Ip = new DevExpress.XtraEditors.SimpleButton();
+            this.RBtn_Oto = new System.Windows.Forms.RadioButton();
+            this.RBtn_Man = new System.Windows.Forms.RadioButton();
+            this.Cbox_NetworkList = new System.Windows.Forms.ComboBox();
+            this.Txt_Ip = new DevExpress.XtraEditors.TextEdit();
+            this.Txt_Mask = new DevExpress.XtraEditors.TextEdit();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Txt_Ip.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Txt_Mask.Properties)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Btn_Set_Ip
+            // 
+            this.Btn_Set_Ip.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Btn_Set_Ip.Appearance.Options.UseFont = true;
+            this.Btn_Set_Ip.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("Btn_Set_Ip.ImageOptions.Image")));
+            this.Btn_Set_Ip.Location = new System.Drawing.Point(59, 149);
+            this.Btn_Set_Ip.Name = "Btn_Set_Ip";
+            this.Btn_Set_Ip.Size = new System.Drawing.Size(101, 48);
+            this.Btn_Set_Ip.TabIndex = 0;
+            this.Btn_Set_Ip.Text = "SET IP";
+            this.Btn_Set_Ip.Click += new System.EventHandler(this.Btn_Set_Ip_Click);
+            // 
+            // RBtn_Oto
+            // 
+            this.RBtn_Oto.AutoSize = true;
+            this.RBtn_Oto.Location = new System.Drawing.Point(59, 19);
+            this.RBtn_Oto.Name = "RBtn_Oto";
+            this.RBtn_Oto.Size = new System.Drawing.Size(81, 17);
+            this.RBtn_Oto.TabIndex = 1;
+            this.RBtn_Oto.TabStop = true;
+            this.RBtn_Oto.Text = "OTOMATİK";
+            this.RBtn_Oto.UseVisualStyleBackColor = true;
+            this.RBtn_Oto.CheckedChanged += new System.EventHandler(this.RBtn_Oto_CheckedChanged);
+            // 
+            // RBtn_Man
+            // 
+            this.RBtn_Man.AutoSize = true;
+            this.RBtn_Man.Location = new System.Drawing.Point(173, 19);
+            this.RBtn_Man.Name = "RBtn_Man";
+            this.RBtn_Man.Size = new System.Drawing.Size(70, 17);
+            this.RBtn_Man.TabIndex = 2;
+            this.RBtn_Man.TabStop = true;
+            this.RBtn_Man.Text = "MANUEL";
+            this.RBtn_Man.UseVisualStyleBackColor = true;
+            this.RBtn_Man.CheckedChanged += new System.EventHandler(this.RBtn_Man_CheckedChanged);
+            // 
+            // Cbox_NetworkList
+            // 
+            this.Cbox_NetworkList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Cbox_NetworkList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Cbox_NetworkList.FormattingEnabled = true;
+            this.Cbox_NetworkList.Location = new System.Drawing.Point(2, 12);
+            this.Cbox_NetworkList.Name = "Cbox_NetworkList";
+            this.Cbox_NetworkList.Size = new System.Drawing.Size(391, 28);
+            this.Cbox_NetworkList.TabIndex = 4;
+            this.Cbox_NetworkList.SelectedIndexChanged += new System.EventHandler(this.Cbox_NetworkList_SelectedIndexChanged);
+            // 
+            // Txt_Ip
+            // 
+            this.Txt_Ip.Location = new System.Drawing.Point(99, 75);
+            this.Txt_Ip.Name = "Txt_Ip";
+            this.Txt_Ip.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Txt_Ip.Properties.Appearance.Options.UseFont = true;
+            this.Txt_Ip.Properties.MaskSettings.Set("MaskManagerType", typeof(DevExpress.Data.Mask.SimpleMaskManager));
+            this.Txt_Ip.Properties.MaskSettings.Set("mask", "999.999.999.999");
+            this.Txt_Ip.Size = new System.Drawing.Size(160, 22);
+            this.Txt_Ip.TabIndex = 5;
+            this.Txt_Ip.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Txt_Ip_PreviewKeyDown);
+            // 
+            // Txt_Mask
+            // 
+            this.Txt_Mask.Location = new System.Drawing.Point(99, 103);
+            this.Txt_Mask.Name = "Txt_Mask";
+            this.Txt_Mask.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Txt_Mask.Properties.Appearance.Options.UseFont = true;
+            this.Txt_Mask.Properties.MaskSettings.Set("MaskManagerType", typeof(DevExpress.Data.Mask.SimpleMaskManager));
+            this.Txt_Mask.Properties.MaskSettings.Set("mask", "999.999.999.999");
+            this.Txt_Mask.Size = new System.Drawing.Size(160, 22);
+            this.Txt_Mask.TabIndex = 6;
+            this.Txt_Mask.Click += new System.EventHandler(this.Txt_Mask_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "MASK";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.RBtn_Oto);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.Btn_Set_Ip);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.RBtn_Man);
+            this.groupBox1.Controls.Add(this.Txt_Mask);
+            this.groupBox1.Controls.Add(this.Txt_Ip);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(24, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(344, 220);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(396, 293);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Cbox_NetworkList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.Text = "IP Transform V1.02";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Txt_Ip.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Txt_Mask.Properties)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton Btn_Set_Ip;
+        private System.Windows.Forms.RadioButton RBtn_Oto;
+        private System.Windows.Forms.RadioButton RBtn_Man;
+        private System.Windows.Forms.ComboBox Cbox_NetworkList;
+        private DevExpress.XtraEditors.TextEdit Txt_Ip;
+        private DevExpress.XtraEditors.TextEdit Txt_Mask;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ IPTransform/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Text.RegularExpressions;
+using DevExpress.XtraEditors;
+using System.Net.NetworkInformation;
+
+namespace IPTransform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+
+            NetworkChange.NetworkAddressChanged += new NetworkAddressChangedEventHandler(NetworkAddressChangedCallback);
+            UpdateNetworkList();
+        }
+
+
+        //NetworkChange.NetworkAddressChanged += new NetworkAddressChangedEventHandler(NetworkAddressChangedCallback);
+        //UpdateNetworkList();
+
+        private void NetworkAddressChangedCallback(object sender, EventArgs e)
+        {
+            UpdateNetworkList();
+        }
+
+        private void UpdateNetworkList()
+        {
+            Cbox_NetworkList.Items.Clear();
+
+            ManagementClass objMC = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection objMOC = objMC.GetInstances();
+
+            Txt_Ip.Text = " ";
+            Txt_Mask.Text = " ";
+
+            foreach (ManagementObject objMO in objMOC)
+            {
+                if ((bool)objMO["IPEnabled"])
+                {
+                    var a = objMO["Caption"].ToString();
+                    a = a.Substring(a.IndexOf("]") + 2);
+                    Cbox_NetworkList.Items.Add(a);
+                }
+            }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //SetIP("Ethernet", "192.168.1.101", "255.255.255.0");
+
+            #region Network donanımlarını cbox a ekler
+
+            //ManagementClass objMC = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            //ManagementObjectCollection objMOC = objMC.GetInstances();
+
+            //foreach (ManagementObject objMO in objMOC)
+            //{
+            //    if ((bool)objMO["IPEnabled"])
+            //    {
+            //        var a = objMO["Caption"].ToString();
+            //        a = a.Substring(a.IndexOf("]")+2);
+            //        Cbox_NetworkList.Items.Add(a);
+            //    }
+            //}
+            
+            #endregion
+        }
+
+
+
+        public static (bool isDhcpEnabled, string[] ipAddresses, string[] subnetMasks) Network_Find(string networkInterface)
+        {
+            ManagementClass objMC = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection objMOC = objMC.GetInstances();
+
+            foreach (ManagementObject objMO in objMOC)
+            {
+                if ((bool)objMO["IPEnabled"] && objMO["Caption"].ToString().Contains(networkInterface))
+                {
+                    bool isDhcpEnabled = false;
+                    if (objMO["DHCPEnabled"] != null)
+                        isDhcpEnabled = (bool)objMO["DHCPEnabled"];
+
+                    string[] ipAddresses = (string[])objMO["IPAddress"];
+                    string[] subnetMasks = (string[])objMO["IPSubnet"];
+
+                    return (isDhcpEnabled, ipAddresses, subnetMasks);
+                }
+            }
+
+            return (false, null, null);
+        }
+
+
+        public static void SetIP(string networkInterface, string ipAddress, string subnetMask) // manuel ip almayı sağlıyor
+        {
+            ManagementClass objMC = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection objMOC = objMC.GetInstances();
+
+            foreach (ManagementObject objMO in objMOC)
+            {
+                if ((bool)objMO["IPEnabled"])
+                {
+                    if (objMO["Caption"].ToString().Contains(networkInterface))
+                    {
+                        ManagementBaseObject objNewIP = null;
+                        ManagementBaseObject objSetIP = null;
+                        ManagementBaseObject objNewGate = null;
+
+                        try
+                        {
+                            objNewIP = objMO.GetMethodParameters("EnableStatic");
+                            objNewIP["IPAddress"] = new string[] { ipAddress };
+                            objNewIP["SubnetMask"] = new string[] { subnetMask };
+
+                            objSetIP = objMO.InvokeMethod("EnableStatic", objNewIP, null);
+                        }
+                        catch (Exception)
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        public static void SetDHCP(string networkInterface) // otomatik ip almayı sağlıyor
+        {
+            ManagementClass objMC = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection objMOC = objMC.GetInstances();
+
+            foreach (ManagementObject objMO in objMOC)
+            {
+                if ((bool)objMO["IPEnabled"])
+                {
+                    if (objMO["Caption"].ToString().Contains(networkInterface))
+                    {
+                        try
+                        {
+                            ManagementBaseObject objNewIP = null;
+                            objNewIP = objMO.InvokeMethod("EnableDHCP", null, null);
+                        }
+                        catch (Exception)
+                        {
+                            throw;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void RBtn_Oto_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RBtn_Oto.Checked)
+            {
+                Txt_Ip.Enabled = false;
+                Txt_Mask.Enabled = false;
+            }
+        }
+
+        private void RBtn_Man_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RBtn_Man.Checked)
+            {
+                Txt_Ip.Enabled = true;
+                Txt_Mask.Enabled = true;
+            }
+        }
+
+        private void Txt_Mask_Click(object sender, EventArgs e)
+        {
+            Txt_Mask.Text = "255.255.255.0";
+        }
+
+        private void Txt_Ip_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Tab)
+            {
+                Txt_Mask.Text = "255.255.255.0";
+            }
+        }
+
+        private void Btn_Set_Ip_Click(object sender, EventArgs e)
+        {
+            if (RBtn_Oto.Checked) // Otomatik ıp
+            {
+                if (Cbox_NetworkList.Text != "")
+                {
+                    SetDHCP(Cbox_NetworkList.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Eksik parametre !", "Uyarı");
+                }
+            }
+
+            if (RBtn_Man.Checked) // Manuel ıp
+            {
+                if (Txt_Ip.Text != "" && Txt_Mask.Text != "")
+                {
+                    SetIP(Cbox_NetworkList.Text,Txt_Ip.Text, Txt_Mask.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Eksik parametre !", "Uyarı");
+                }
+
+            }
+        }
+
+        private void Cbox_NetworkList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var result_dhcp = Network_Find(Cbox_NetworkList.Text).isDhcpEnabled;
+            var result_ip = Network_Find(Cbox_NetworkList.Text).ipAddresses[0];
+            var result_mask = Network_Find(Cbox_NetworkList.Text).subnetMasks[0];
+            if (result_dhcp) 
+            {
+                RBtn_Oto.Checked = true;
+            }
+            else
+            {
+                RBtn_Man.Checked = true;
+            }
+            Txt_Ip.Text = result_ip;
+            Txt_Mask.Text = result_mask;
+
+
+        }
+
+    }
+}
